6240eea917b2a22f6c70e0c1a70cd43b7da2a1fb
Hanyue Zhang <hxz961@student.bham.ac.uk>
2020-06-14 16:02:02 +0100

    tada template

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('', views.index, name='index'),
+    path('post/<int:pk>/', views.content, name='content'),
+    path('cv', views.cv, name='cv'),
+    path('about', views.about, name='about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,9 +3,13 @@ from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 
-def post_list(request):
+def index(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
-def post_detail(request, pk):
+    return render(request, 'blog/index.html', {'posts': posts})
+def content(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    return render(request, 'blog/content.html', {'post': post})
+def cv(request):
+    return render(request, 'blog/cv.html', {})
+def about(request):
+    return render(request, 'blog/about.html', {})
