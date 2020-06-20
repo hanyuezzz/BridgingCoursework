8fa28d5413e4fb1f762c4d1176894412814b92d8
Hanyue Zhang <hxz961@student.bham.ac.uk>
2020-06-20 14:50:57 +0100

    fixed the problem of posts not showing

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('post/<int:pk>/', views.content, name='content'),
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_content, name='post_content'),
     path('cv', views.cv, name='cv'),
     path('about', views.about, name='about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,12 +3,12 @@ from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 
-def index(request):
+def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/index.html', {'posts': posts})
-def content(request, pk):
+    return render(request, 'blog/post_list.html', {'posts': posts})
+def post_content(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/content.html', {'post': post})
+    return render(request, 'blog/post_content.html', {'post': post})
 def cv(request):
     return render(request, 'blog/cv.html', {})
 def about(request):
